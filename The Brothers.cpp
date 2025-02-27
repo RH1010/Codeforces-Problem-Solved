ab49251eb2d4e841c4695fd1686837f2609712fb
Rasel Hasan <55950753+RH1010@users.noreply.github.com>
2025-02-27 09:06:04 +0600

    Add files via upload

---
+++ The Brothers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string F1, S1;
+    cin>> F1 >> S1;
+
+    string F2, S2;
+    cin>> F2 >> S2;
+
+    cout << (S1 == S2 ?  "ARE Brothers" : "NOT") << endl;
+
+}
+
